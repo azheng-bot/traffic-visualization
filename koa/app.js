d28814d6da54a74b04a0dd226ad756fd0a1e5d74
xuejiahao <763618549@qq.com>
2022-01-31 15:22:55 +0800

    跨域问题

--- koa/app.js
+++ koa/app.js
@@ -2,8 +2,13 @@ const PORT = 8000;
 
 const Koa = require('koa');
 const app = new Koa();
-const routers = require("./routers/index")
 
+// 跨域问题
+const cors = require("koa-cors")
+app.use(cors())
+
+// 路由
+const routers = require("./routers/index")
 app.use(routers.routes());
 
 app.listen(PORT, () => {
