f04e92b33ecd48b72066d96bb62378a19c9936ee
pangjiancongl <1132618513@qq.com>
2022-02-05 11:46:25 +0800

    基本图标

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.less'
-import App from './App'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MyCity from './views/maps/MyCity/index'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.less";
+import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MyCity from "./views/maps/MyCity/index";
+import Icon from "./views/knowledgeModule/Icon/index";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} >
+        <Route path="/" element={<App />}>
           <Route path="/" element={<MyCity />}></Route>
           <Route path="/mycity" element={<MyCity />}></Route>
+          <Route path="/icon" element={<Icon />}></Route>
         </Route>
-
       </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
