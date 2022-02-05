125efcfa2f458ab3da183e91f5d393ace829d952
pangjiancongl <1132618513@qq.com>
2022-02-05 11:46:10 +0800

    封装请求

---
+++ react/src/utils/request.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const baseURL = "http://localhost:8000/";
+const instanceWithoutToken = axios.create({ baseURL });
+instanceWithoutToken.interceptors.response.use(
+    (response) => {
+        if (response.status == 200) {
+            return response;
+        }
+    },
+    (error) => new Promise.reject(error)
+);
+// 响应拦截器
+instanceWithoutToken.interceptors.response.use((response) => response.data);
+const generateRequestConfig = (url, method, data) => ({
+    url,
+    method,
+    [method.toLowerCase() === "get" ? "params" : "data"]: data,
+});
+// 请求函数
+export function request(url, method, data) {
+    return instanceWithoutToken(generateRequestConfig(url, method, data));
+}
+
