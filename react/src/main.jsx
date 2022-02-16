d262628ebb42394009a721f06329f488bba0acfd
xuejiahao <763618549@qq.com>
2022-02-16 12:42:47 +0800

    .

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -4,15 +4,18 @@ import "./index.less";
 import App from "./App";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// 我的城市 - MyCity
 import MyCity from "./views/MyCity/index";
-import Knowledge from "./views/Knowledge/index";
-import Deep from "./views/Deep/index";
-import Future from "./views/Future/index";
-import Sign from "./views/Knowledge/Sign/index";
 import Bus from "./views/MyCity/bus/index";
+import Subway from "./views/MyCity/Subway/index";
+// 基础通识 - Knowledge
+import Knowledge from "./views/Knowledge/index";
+import Sign from "./views/Knowledge/Sign/index";
+// 深入交通 - Deep
+import Deep from "./views/Deep/index";
+// 未来展望 - Future
+import Future from "./views/Future/index";
 
-import Bus from "./views/mycityModule/bus/index";
-import Metro from "./views/mycityModule/metro/index";
 ReactDOM.render(
   <React.StrictMode>
     <Router>
@@ -21,8 +24,10 @@ ReactDOM.render(
           <Route path="/" element={<MyCity />}></Route>
           <Route path="/mycity" element={<MyCity />}>
             <Route path="bus" element={<Bus />}></Route>
+            <Route path="subway" element={<Subway />}></Route>
           </Route>
           <Route path="/knowledge" element={<Knowledge />}>
+            <Route path="sign" element={<Sign />}></Route>
             <Route path="sign" element={<Sign />}></Route>
           </Route>
           <Route path="/deep" element={<Deep />}>
@@ -31,8 +36,8 @@ ReactDOM.render(
           <Route path="/future" element={<Future />}>
             <Route path="ai" element={<Sign />}></Route>
           </Route>
-          <Route path="/bus" element={<Bus />}></Route>
-          <Route path="/metro/:id" element={<Metro />}></Route>
+          {/* <Route path="/bus" element={<Bus />}></Route>
+          <Route path="/metro/:id" element={<Metro />}></Route> */}
         </Route>
       </Routes>
     </Router>
